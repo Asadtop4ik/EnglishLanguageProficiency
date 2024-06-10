dd7c61038e04dd3f166aa20fa9c89dbb8ebb09ca
asadtop4ik <asadbek.backend@gmail.com>
2024-06-11 02:03:37 +0500

    updated

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0s45)9v+s(42@xs-=x8d12(u*1kri)9t#htqb&v4u^)e5t()h0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['englishlanguageproficiency.onrender.com', '127.0.0.1']
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -68,6 +68,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+PORT = int(os.getenv('PORT', '8000'))
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
